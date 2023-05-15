b4aec4f7f1f21600808c40fdf25a8d64ec71913b
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2023-05-15 14:24:05 +0800

    Update env.php

--- core/env.php
+++ core/env.php
@@ -3,7 +3,12 @@
 define('BASE_URL', 'http://localhost/jesm/');
 define("BASE_PATH", __DIR__ . "/../");
 
+// define("HOST", "localhost");
+// define("USER", "root");
+// define("PASSWORD", "");
+// define("DBNAME", "jesm_db");
+
 define("HOST", "localhost");
-define("USER", "root");
-define("PASSWORD", "");
-define("DBNAME", "jesm_db");
+define("USER", "u981310152_jesm_root");
+define("PASSWORD", "7a0z38[^Ku");
+define("DBNAME", "u981310152_jesm_db");
